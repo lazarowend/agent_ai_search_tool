cb38c5d5966405f9d8f428dd98a78bf589d165a4
lazarowend <lazaroclans1@gmail.com>
2024-12-11 09:09:15 -0300

    add agent and prompt chat

--- agent.py
+++ agent.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
 from langchain_community.tools import WikipediaQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
-
+from langchain_experimental.agents.agent_toolkits import create_python_agent
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
@@ -21,3 +22,24 @@ wikipedia_tool = WikipediaQueryRun(
         lang='pt'
     )
 )
+
+agent_executor = create_python_agent(
+    llm=model,
+    tool=wikipedia_tool,
+    verbose=True
+)
+
+prompt_template = PromptTemplate(
+    input_variables=['query'],
+    template='''
+    Pesquise na web sobre {query} e forneça um resumo sobre,
+    responda tudo em pt-br
+    '''
+)
+
+query = 'Alan Turing'
+
+prompt = prompt_template.format(query=query)
+
+response = agent_executor.invoke(prompt)
+print(response.get('output'))
